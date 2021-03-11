7c288703e58ec69e69ad41283172fd4da56e3339
dudgu <dudgus22342@naver.com>
2021-03-11 11:05:05 +0900

    address들

--- src/main/java/com/kh/spring/address/model/service/AddressServiceImpl.java
+++ src/main/java/com/kh/spring/address/model/service/AddressServiceImpl.java
@@ -1,9 +1,36 @@
 package com.kh.spring.address.model.service;
 
+import java.util.ArrayList;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.kh.spring.address.model.dao.AddressDao;
+import com.kh.spring.address.model.vo.Address;
+import com.kh.spring.address.model.vo.PageInfo;
 
 @Service("addressService")
 public class AddressServiceImpl implements AddressService {
+
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	@Autowired
+	private AddressDao addressDao;
+	
+	@Override
+	public int selectListCount() {
+		// TODO Auto-generated method stub
+		return addressDao.selectListCount(sqlSession);
+	}
+
+	@Override
+	public ArrayList<Address> selectListCount(PageInfo pi) {
+		// TODO Auto-generated method stub
+		return addressDao.selectListCount(sqlSession,pi);
+	}
 
 	
 	
---
+++ src/main/java/com/kh/spring/work/controller/WorkController.java
@@ -0,0 +1,40 @@
+package com.kh.spring.work.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonIOException;
+
+
+
+@Controller
+public class WorkController {
+	
+
+
+	@RequestMapping("view.work")
+	public String viewAddress() {
+		return "work/work";
+	}
+	
+	
+	////////////////시작
+}
