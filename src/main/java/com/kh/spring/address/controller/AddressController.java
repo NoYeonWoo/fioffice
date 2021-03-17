766e512fd37a2623e5315ebaf27d50d90ca78953
dudgu <dudgus22342@naver.com>
2021-03-17 09:52:46 +0900

    address 컨트롤러 커밋  바티스 컨피그 커밋

--- src/main/java/com/kh/spring/address/controller/AddressController.java
+++ src/main/java/com/kh/spring/address/controller/AddressController.java
@@ -25,13 +25,62 @@ import com.google.gson.JsonIOException;
 
 
 import com.kh.spring.address.model.service.AddressService;
+import com.kh.spring.address.model.vo.Address;
+import com.kh.spring.address.model.vo.PageInfo;
+import com.kh.spring.common.Pagination;
 
+@Controller
 public class AddressController {
 	
 	@Autowired
 	private AddressService addressService;
 
+	@RequestMapping("view.add")
+	public String selectList(@RequestParam(value="currentPage",required=false, defaultValue="1") int currentPage, Model model) {
+		
+		
+	
+		int listCount = addressService.selectListCount();
+		//System.out.println(listCount);
+		
+		PageInfo pi = Pagination.getPageInfo(listCount, currentPage,10,5);//페이지리밋 10 보드 이미지 5
+		
+		ArrayList<Address> list = addressService.selectListCount(pi);
+		
+		model.addAttribute("list",list);
+		model.addAttribute("pi",pi);
+		
+
+		return "address/addressView3";//3
+		
+	}
+	
+	@RequestMapping("update.add")
+	public String viewUpdate( ){
+		
+
+		return "address/addressUpdate";//3
+		
+	}
+	
+	@RequestMapping("detail.add")
+	public String viewDetail( ){
+		
+
+		return "address/addressDetail";//3
+		
+	}
+	@RequestMapping("insert.add")
+	public String viewinsert( ){
+		
+
+		return "address/addressInsert";//3
+		
+	}
 	
 	
 	////////////////시작
 }
+
+
+
