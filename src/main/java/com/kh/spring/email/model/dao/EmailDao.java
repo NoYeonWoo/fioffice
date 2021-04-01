dc52290d8435f77c7fc8f2383236e3b408a954aa
dudgu <dudgus22342@naver.com>
2021-04-01 16:04:32 +0900

    email들 많이

--- src/main/java/com/kh/spring/email/model/dao/EmailDao.java
+++ src/main/java/com/kh/spring/email/model/dao/EmailDao.java
@@ -8,10 +8,25 @@ import org.springframework.stereotype.Repository;
 
 import com.kh.spring.address.model.vo.PageInfo;
 import com.kh.spring.email.model.vo.Email;
+import com.kh.spring.email.model.vo.EmailInfo;
 import com.kh.spring.employee.model.vo.Employee;
 
 @Repository("emailDao")
 public class EmailDao {
+	
+	
+	public ArrayList<EmailInfo> selectMainEmail(SqlSessionTemplate sqlSession, Employee emp) {
+		// TODO Auto-generated method stub
+		return (ArrayList)sqlSession.selectList("EmailMapper.selectUser",emp);
+	}
+	
+	
+	
+	///////////////////////
+	public int selectListCount(SqlSessionTemplate sqlSession) {/////////////////ㅅ 정
+		// TODO Auto-generated method stub
+		return sqlSession.selectOne("emailMapper.selectListCount");
+	}
 
 	public ArrayList<Email> selectListCount(SqlSessionTemplate sqlSession, PageInfo pi, Employee emp) {
 		// TODO Auto-generated method stub
@@ -19,11 +34,54 @@ public class EmailDao {
 		RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
 		return (ArrayList)sqlSession.selectList("emailMapper.selectList", emp, rowBounds); 
 	}
-
-	public int selectListCount(SqlSessionTemplate sqlSession) {
+//////////////////////////////////////
+	public ArrayList<EmailInfo> selectUser(SqlSessionTemplate sqlSession, Employee emp) {
 		// TODO Auto-generated method stub
-		return sqlSession.selectOne("emailMapper.selectListCount");
+		return (ArrayList)sqlSession.selectList("emailMapper.selectUser",emp);
 	}
+
+	
+	/////////////////////////
+	
+	public Email selectUserEmail(SqlSessionTemplate sqlSession, String empNo) {
+		// TODO Auto-generated method stub
+		return sqlSession.selectOne("emailMapper.selectUser",empNo);
+	}
+	
+	public EmailInfo selectUserEmailInfo(SqlSessionTemplate sqlSession, String empNo) {
+		// TODO Auto-generated method stub
+		return sqlSession.selectOne("emailMapper.selectUser",empNo);
+	}
+
+	public EmailInfo selectUserInfo2(SqlSessionTemplate sqlSession, Employee emp) {
+		// TODO Auto-generated method stub
+		return sqlSession.selectOne("emailMapper.selectUserInfo",emp);
+	}
+
+	public Email selectUser2(SqlSessionTemplate sqlSession, Employee emp) {
+		// TODO Auto-generated method stub
+		return sqlSession.selectOne("emailMapper.selectUser",emp);
+	}
+
+	public int insertemail(SqlSessionTemplate sqlSession, Email email) {
+		// TODO Auto-generated method stub
+		return sqlSession.insert("emailMapper.insertEmail",email);
+	}
+
+
+
+	public Email selectEmailDetail(SqlSessionTemplate sqlSession, int eno) {
+		// TODO Auto-generated method stub
+		return sqlSession.selectOne("emailMapper.selectEmailDetail",eno);
+	}
+
+
+
+	public int deleteEmail(SqlSessionTemplate sqlSession, int eno) {
+		// TODO Auto-generated method stub
+		return sqlSession.update("emailMapper.deleteEmail",eno);
+	}
+
 
 
 	/*
